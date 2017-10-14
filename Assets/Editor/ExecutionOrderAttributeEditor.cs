39e98365f6477b80a4489494b32e068a74c24bfd
azixMcAze <mathieu.leber@gmail.com>
2017-10-14 20:50:20 +0200

    Added ExecutionOrderAttribute and related editor

---
+++ Assets/Editor/ExecutionOrderAttributeEditor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.Reflection;
+
+public static class ExecutionOrderAttributeEditor
+{
+	[UnityEditor.Callbacks.DidReloadScripts]
+	static void OnDidReloadScripts()
+	{
+		AssetDatabase.StartAssetEditing();
+		UpdateExecutionOrder();
+		AssetDatabase.StopAssetEditing();
+	}
+
+	static void UpdateExecutionOrder()
+	{
+		var scripts = MonoImporter.GetAllRuntimeMonoScripts();
+		foreach(var script in scripts)
+		{
+			var type = script.GetClass();
+			if(type != null && Attribute.IsDefined(type, typeof(ExecutionOrderAttribute)))
+			{
+				var attribute = (ExecutionOrderAttribute)Attribute.GetCustomAttribute(type, typeof(ExecutionOrderAttribute));
+				int order = attribute.order;
+				if(MonoImporter.GetExecutionOrder(script) != order)
+					MonoImporter.SetExecutionOrder(script, order);
+			}
+		}
+	}
+}
---
+++ Assets/ExecutionOrderAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class ExecutionOrderAttribute : System.Attribute
+{
+	public int order
+	{
+		get;
+		private set;
+	}
+
+	public ExecutionOrderAttribute(int order)
+	{
+		this.order = order;
+	}
+}
