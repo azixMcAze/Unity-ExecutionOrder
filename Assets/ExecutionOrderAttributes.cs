6c58462f5bd295f933e2fcfc0da444bd061b7514
azixMcAze <mathieu.leber@gmail.com>
2017-10-14 20:59:17 +0200

    Created ExecuteAfter and ExecuteBefore attributes
    Renamed ExecutionOrderAttribute.cs and ExecutionOrderAttributeEditor.cs
    files
    Created ExecuteAfterAttribute and ExecuteBeforeAttribute classes

---
+++ Assets/ExecutionOrderAttributes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class ExecutionOrderAttribute : System.Attribute
+{
+	public int order { get; private set; }
+
+	public ExecutionOrderAttribute(int order)
+	{
+		this.order = order;
+	}
+}
+
+[AttributeUsage(AttributeTargets.Class)]
+public class ExecuteAfterAttribute : System.Attribute
+{
+	public Type type { get; private set; }
+	public int orderIncrease { get; private set; }
+
+	public ExecuteAfterAttribute(Type type, int orderIncrease = 10)
+	{
+		this.type = type;
+		this.orderIncrease = orderIncrease;
+	}
+}
+
+[AttributeUsage(AttributeTargets.Class)]
+public class ExecuteBeforeAttribute : System.Attribute
+{
+	public Type type { get; private set; }
+	public int orderDecrease { get; private set; }
+
+	public ExecuteBeforeAttribute(Type type, int orderDecrease = 10)
+	{
+		this.type = type;
+		this.orderDecrease = orderDecrease;
+	}
+}
