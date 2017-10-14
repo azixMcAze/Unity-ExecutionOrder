70e079179a782a830623d10a7de77aff53df158b
azixMcAze <mathieu.leber@gmail.com>
2017-10-14 20:50:31 +0200

    Added test scripts

---
+++ Assets/AttributeTest1.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecutionOrder(1)]
+public class AttributeTest1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/AttributeTest2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecutionOrder(2)]
+public class AttributeTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
