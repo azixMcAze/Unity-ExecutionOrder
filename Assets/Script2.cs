32b792f1849da0ffe6293271dc4900102df0994d
azixMcAze <mathieu.leber@gmail.com>
2017-10-21 21:16:58 +0200

    Replaced the test scripts with the examples from the README

---
+++ Assets/Script2.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// this script will have an execution order of 60 (50+10)
+[ExecuteAfter(typeof(Script1))]
+public class Script2 : MonoBehaviour {
+    void Start () {
+        Debug.Log("Second");
+    }
+}
